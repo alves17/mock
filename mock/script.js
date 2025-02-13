aaea489ab1329fd592b52acced057db4e360be86
alves17 <142310451+alves17@users.noreply.github.com>
2025-02-13 08:34:33 -0300

    Add files via upload

---
+++ mock/script.js
@@ -0,0 +1,37 @@
+let nome = "Rayssa";
+const idade = 17;
+var cidade = "Toledo";
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Cidade: ", cidade);
+
+let texto = "Estou aprendendo JavaScript!";
+let numero = 2025;
+let verdadeiro = true;
+
+console.log(typeof texto, texto);
+console.log(typeof numero, numero);
+console.log(typeof verdadeiro, verdadeiro);
+
+
+let cor = "azul";
+console.log("Cor inicial: ", cor);
+cor = "vermelho";
+console.log("Nova cor: ", cor);
+
+
+
+let carro ="vermelho"
+console.log("Cor inicial: ", cor);
+cor = "rosa";
+console.log("Nova cor: ", cor);
+
+
+
+const carro ="azul"
+console.log("Cor inicial: ", cor)
+cor ="vermelho"
+console.log("Nova cor ", cor)
+
+
